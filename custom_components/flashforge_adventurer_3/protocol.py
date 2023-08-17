bd5c5c51bae57e4a3618557520b2459017501e13
Torsten Juul-Jensen <tjuuljensen@gmail.com>
2023-08-17 10:47:07 +0200

    Add files via upload

---
+++ custom_components/flashforge_adventurer_3/protocol.py
@@ -0,0 +1,82 @@
+import asyncio
+from codecs import StreamReader, StreamWriter
+import logging
+import os
+import re
+from typing import Optional, Tuple, TypedDict
+
+logger = logging.getLogger(__name__)
+
+BUFFER_SIZE = 1024
+TIMEOUT_SECONDS = 5
+STATUS_COMMAND = '~M601 S1'
+PRINT_JOB_INFO_COMMAND = '~M27'
+TEMPERATURE_COMMAND = '~M105'
+
+STATUS_REPLY_REGEX = re.compile('CMD M27 Received.\r\n\w+ printing byte (\d+)/(\d+)\r\n(.*?)ok\r\n')
+TEMPERATURE_REPLY_REGEX = re.compile('CMD M105 Received.\r\nT0:(\d+)\W*/(\d+) B:(\d+)\W*/(\d+)\r\n(.*?)ok\r\n')
+
+class PrinterStatus(TypedDict):
+    online: bool
+    printing: Optional[bool]
+    progress: Optional[int]
+    bed_temperature: Optional[int]
+    desired_bed_temperature: Optional[int]
+    nozzle_temperature: Optional[int]
+    desired_nozzle_temperature: Optional[int]
+
+
+async def send_msg(reader: StreamReader, writer: StreamWriter, payload: str):
+    msg = f'{payload}\r\n'
+    writer.write(msg.encode())
+    logger.debug(f'Sent "{payload}" to the printer')
+    await writer.drain()
+    result = await reader.read(BUFFER_SIZE)
+    logger.debug(f'Response from the printer: {result}')
+    return result.decode()
+
+
+async def collect_data(ip: str, port: int) -> Tuple[PrinterStatus, Optional[str], Optional[str]]:
+    future = asyncio.open_connection(ip, port)
+    try:
+        reader, writer = await asyncio.wait_for(future, timeout=TIMEOUT_SECONDS)
+    except (asyncio.TimeoutError, OSError):
+        return { 'online': False }, None, None
+    response: PrinterStatus = { 'online': True }
+    await send_msg(reader, writer, STATUS_COMMAND)
+    print_job_info = await send_msg(reader, writer, PRINT_JOB_INFO_COMMAND)
+    temperature_info = await send_msg(reader, writer, TEMPERATURE_COMMAND)
+    writer.close()
+    await writer.wait_closed()
+    return response, print_job_info, temperature_info
+
+
+def parse_data(response: PrinterStatus, print_job_info: str, temperature_info: str) -> PrinterStatus:
+    print_job_info_match = STATUS_REPLY_REGEX.match(print_job_info)
+    if print_job_info_match:
+        current = int(print_job_info_match.group(1))
+        total = int(print_job_info_match.group(2))
+        response['progress'] = int(current / total * 100)
+    temperature_match = TEMPERATURE_REPLY_REGEX.match(temperature_info)
+    if temperature_match:
+        # Printer is printing if desired temperatures are greater than zero. If not, it's paused.
+        desired_nozzle_temperature = int(temperature_match.group(2))
+        desired_bed_temperature = int(temperature_match.group(4))
+        response['printing'] = bool(desired_nozzle_temperature and desired_bed_temperature)
+        response['nozzle_temperature'] = int(temperature_match.group(1))
+        response['desired_nozzle_temperature'] = desired_nozzle_temperature
+        response['bed_temperature'] = int(temperature_match.group(3))
+        response['desired_bed_temperature'] = desired_bed_temperature
+    return response
+
+
+async def get_print_job_status(ip: str, port: int) -> PrinterStatus:
+    response, print_job_info, temperature_info = await collect_data(ip, port)
+    if not response['online']:
+        return response
+    return parse_data(response, print_job_info, temperature_info)
+
+
+if __name__ == '__main__':
+    status = asyncio.run(get_print_job_status(os.environ['PRINTER_IP'], 8899))
+    print(status)
---
+++ custom_components/flashforge_adventurer_3/sensor.py
@@ -0,0 +1,154 @@
+from datetime import timedelta
+import logging
+from typing import Any, Callable, Dict, Optional, TypedDict
+
+import async_timeout
+from homeassistant import config_entries, core
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+)
+import voluptuous as vol
+
+from .const import DOMAIN
+from .protocol import get_print_job_status
+
+LOGGER = logging.getLogger(__name__)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required('ip'): cv.string,
+        vol.Required('port'): cv.string,
+    }
+)
+
+class PrinterDefinition(TypedDict):
+    ip: str
+    port: int
+
+
+async def async_setup_entry(
+    hass: core.HomeAssistant,
+    config_entry: config_entries.ConfigEntry,
+    async_add_entities: Callable,
+) -> bool:
+    config = hass.data[DOMAIN][config_entry.entry_id]
+    if config_entry.options:
+        config.update(config_entry.options)
+    coordinator = FlashforgeAdventurer3Coordinator(hass, config)
+    await coordinator.async_config_entry_first_refresh()
+    sensors = [
+        FlashforgeAdventurer3StateSensor(coordinator, config),
+        FlashforgeAdventurer3ProgressSensor(coordinator, config),
+    ]
+    async_add_entities(sensors, update_before_add=True)
+
+
+class FlashforgeAdventurer3Coordinator(DataUpdateCoordinator):
+    def __init__(self, hass, printer_definition: PrinterDefinition):
+        super().__init__(
+            hass,
+            LOGGER,
+            name='My sensor',
+            update_interval=timedelta(seconds=60),
+        )
+        self.ip = printer_definition['ip_address']
+        self.port = printer_definition['port']
+
+    async def _async_update_data(self):
+        async with async_timeout.timeout(5):
+            return await get_print_job_status(self.ip, self.port)
+
+
+class FlashforgeAdventurer3CommonPropertiesMixin:
+    @property
+    def name(self) -> str:
+        return f'FlashForge Adventurer 3'
+
+    @property
+    def unique_id(self) -> str:
+        return f'flashforge_adventurer_3_{self.ip}'
+
+
+class BaseFlashforgeAdventurer3Sensor(FlashforgeAdventurer3CommonPropertiesMixin, CoordinatorEntity, Entity):
+    def __init__(self, coordinator: DataUpdateCoordinator, printer_definition: PrinterDefinition) -> None:
+        super().__init__(coordinator)
+        self.ip = printer_definition['ip_address']
+        self.port = printer_definition['port']
+        self._available = False
+        self.attrs = {}
+
+    @property
+    def state(self) -> Optional[str]:
+        return self._state
+
+    @property
+    def device_state_attributes(self) -> Dict[str, Any]:
+        return self.attrs
+
+    @property
+    def extra_state_attributes(self) -> Dict[str, Any]:
+        return self.attrs
+
+    @callback
+    def _handle_coordinator_update(self) -> None:
+        self.attrs = self.coordinator.data
+        self.async_write_ha_state()
+
+
+class FlashforgeAdventurer3StateSensor(BaseFlashforgeAdventurer3Sensor):
+    @property
+    def name(self) -> str:
+        return f'{super().name} state'
+
+    @property
+    def unique_id(self) -> str:
+        return f'{super().unique_id}_state'
+
+    @property
+    def available(self) -> bool:
+        return True
+
+    @property
+    def state(self) -> Optional[str]:
+        if self.attrs.get('online'):
+            if self.attrs.get('printing'):
+                return 'printing'
+            else:
+                return 'online'
+        else:
+            return 'offline'
+
+    @property
+    def icon(self) -> str:
+        return 'mdi:printer-3d'
+
+
+class FlashforgeAdventurer3ProgressSensor(BaseFlashforgeAdventurer3Sensor):
+    @property
+    def name(self) -> str:
+        return f'{super().name} progress'
+
+    @property
+    def unique_id(self) -> str:
+        return f'{super().unique_id}_progress'
+
+    @property
+    def available(self) -> bool:
+        return bool(self.attrs.get('online'))
+
+    @property
+    def state(self) -> Optional[str]:
+        return self.attrs.get('progress', 0)
+
+    @property
+    def icon(self) -> str:
+        return 'mdi:percent-circle'
+
+    @property
+    def unit_of_measurement(self) -> str:
+        return '%'
